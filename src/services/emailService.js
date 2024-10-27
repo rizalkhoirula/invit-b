49fbe6c6ba0eaadc44959c87433d2ae258a4432c
Rizal KA <112916824+rizalkhoirula@users.noreply.github.com>
2024-10-27 20:13:20 +0800

    Update emailService.js

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -9,6 +9,10 @@ const sendEmailWithQRCode = async (name, email, link) => {
         const qrCodeData = await QRCode.toDataURL(link);
         console.log('QR Code generated.');
 
+        // Mengonversi Data URL menjadi buffer
+        const base64Data = qrCodeData.split(',')[1];
+        const qrCodeBuffer = Buffer.from(base64Data, 'base64');
+
         let transporter = nodemailer.createTransport({
             host: 'smtp.zoho.com',
             port: 587,
@@ -26,9 +30,15 @@ const sendEmailWithQRCode = async (name, email, link) => {
             html: `
                 <p>Hai ${name},</p>
                 <p>Terima kasih telah mengisi formulir undangan. Silakan gunakan QR code di bawah ini untuk check-in pada hari acara:</p>
-                <p><img src="${qrCodeData}" alt="QR Code" /></p>
                 <p>Kami menunggu kehadiran Anda!</p>
             `,
+            attachments: [
+                {
+                    filename: 'qrcode.png',
+                    content: qrCodeBuffer,
+                    cid: 'qrcode' 
+                }
+            ]
         };
 
         await transporter.sendMail(mailOptions);
